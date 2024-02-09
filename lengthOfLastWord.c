993f70ad8ab017d47b65a6ae50ce0e23bf7a7c82
millieba <millieba@hotmail.com>
2024-02-09 23:30:14 +0100

    Add main function and autoformat lengthOfLastWord.c

--- lengthOfLastWord.c
+++ lengthOfLastWord.c
@@ -1,18 +1,31 @@
 // https://leetcode.com/problems/length-of-last-word/description/
+#include <stdio.h>
+#include <string.h>
 
-int lengthOfLastWord(char* s) {
+int lengthOfLastWord(char *s)
+{
     int found_space = 0;
     int l = 0;
-    for (int i=0;i<strlen(s); i++){
-        if (s[i]==' '){
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == ' ')
+        {
             found_space = 1;
             continue;
-        } else if (found_space) {
+        }
+        else if (found_space)
+        {
             found_space = 0;
             l = 0;
         }
-        
-        l++;        
+
+        l++;
     }
     return l;
+};
+
+void main()
+{
+    char s[] = "hello world     ";
+    printf("%d\n", lengthOfLastWord(s));
 }
