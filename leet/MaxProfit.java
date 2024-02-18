f057f6acd31abe7841e7626113e4f9b89eecb192
millieba <millieba@hotmail.com>
2024-02-18 16:12:33 +0100

    Solve max profit in java

---
+++ leet/MaxProfit.java
@@ -0,0 +1,19 @@
+package leet;
+
+public class MaxProfit {
+    public static int maxProfit(int[] prices) {
+        int totalProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i]) { // If tomorrow's price is higher than today's
+                totalProfit += prices[i + 1] - prices[i]; // Profit = profit for selling next day - buying price
+            }
+        }
+        System.out.println(totalProfit);
+        return totalProfit;
+    }
+
+    public static void main(String[] args) {
+        maxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+    }
+
+}
